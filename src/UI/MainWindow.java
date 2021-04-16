4832d9c333fd829b92531041df3b7fe08db27878
Alexander Hiermann <8108@htl.rennweg.at>
2021-04-16 11:29:07 +0200

    Optimized MainWindow and LoginWindow

--- src/UI/MainWindow.java
+++ src/UI/MainWindow.java
@@ -1,11 +1,4 @@
 package UI;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class MainWindow extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
-
-    }
+public class MainWindow {
 }
