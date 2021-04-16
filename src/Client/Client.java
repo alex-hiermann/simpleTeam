474ae5e73b2310cf82e10154675ef0c0af584e5b
Maximilian Burger <mabug@outlook.de>
2021-04-16 20:32:46 +0200

    Snapshot for testing

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,18 +1,12 @@
 package Client;
 
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.Socket;
 
-public class Client extends Application implements Runnable {
+public class Client implements Runnable {
 
     Socket socket;
     User user;
@@ -26,7 +20,6 @@ public class Client extends Application implements Runnable {
     @Override
     public void run() {
         System.out.println("Hello I'm " + user.getUsername());
-        launch();
         try {
             DataInputStream dis = new DataInputStream(socket.getInputStream());
             String data = "";
@@ -39,13 +32,4 @@ public class Client extends Application implements Runnable {
 
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root2 = FXMLLoader.load(getClass().getResource("../UI/MainWindow.fxml"));
-        Stage stage2 = new Stage();
-        stage2.getIcons().add(new Image(this.getClass().getResourceAsStream("/icon-1.png")));
-        stage2.setTitle("SimpleTeam");
-        stage2.setScene(new Scene(root2));
-        stage2.show();
-    }
 }
--- src/Client/ClientMain.java
+++ src/Client/ClientMain.java
@@ -7,9 +7,13 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.Socket;
 
 public class ClientMain extends Application {
+
+    Stage currentStage = new Stage();
+
     public static void main(String[] args) {
         System.out.println("Initializing Simple Team");
         launch(args);
@@ -29,7 +33,6 @@ public class ClientMain extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../UI/LoginWindow.fxml"));
-        Parent root2 = FXMLLoader.load(getClass().getResource("../UI/MainWindow.fxml"));
 
         Scene scene = new Scene(root);
         stage.getIcons().add(new Image(this.getClass().getResourceAsStream("/icon-1.png")));
@@ -37,10 +40,19 @@ public class ClientMain extends Application {
         stage.setScene(scene);
         stage.show();
 
+        currentStage = stage;
+
+    }
+
+    public void showMainWindow() throws IOException {
+        Parent root2 = FXMLLoader.load(getClass().getResource("../UI/MainWindow.fxml"));
         Stage stage2 = new Stage();
         stage2.getIcons().add(new Image(this.getClass().getResourceAsStream("/icon-1.png")));
         stage2.setTitle("SimpleTeam");
         stage2.setScene(new Scene(root2));
         stage2.show();
+
+        currentStage = stage2;
     }
+
 }
