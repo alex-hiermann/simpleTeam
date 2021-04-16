277eeb49990e346e3dd7b11c15cb93dc7ee003a9
Maximilian Burger <mabug@outlook.de>
2021-04-16 11:04:39 +0200

    Added Basic UI functionality

--- src/Client/ClientMain.java
+++ src/Client/ClientMain.java
@@ -1,3 +1,5 @@
+package Client;
+
 import Client.Client;
 import Client.User;
 import javafx.application.Application;
@@ -7,37 +9,28 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.net.Socket;
-import java.util.Scanner;
 
-public class Main extends Application {
+public class ClientMain extends Application {
     public static void main(String[] args) {
-        launch(args);
         System.out.println("Initializing Simple Team");
-        System.out.print("LogIn: ");
-        Scanner sc = new Scanner(System.in);
-        String username = sc.nextLine();
+        launch(args);
 
-        User user = new User(username);
+    }
 
-        System.out.println();
-        System.out.print("Enter a valid IP for the server: ");
-        Scanner sc1 = new Scanner(System.in);
-        String ip = sc1.nextLine();
+    public static Socket connectToServer(String ip) {
         try {
             Socket s = new Socket(ip.split(":")[0], Integer.parseInt(ip.split(":")[1]));
             System.out.println("Connection successful: " + s.toString());
-            Client client = new Client(s, user);
-            Thread thread = new Thread(client);
-            thread.start();
+            return s;
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
     }
 
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("UI/LoginWindow.fxml"));
-
         Scene scene = new Scene(root);
         stage.setTitle("Login");
         stage.setScene(scene);
--- src/UI/LoginWindow.java
+++ src/UI/LoginWindow.java
@@ -1,4 +1,28 @@
 package UI;
 
+import Client.ClientMain;
+import Client.Client;
+import Client.User;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import java.net.Socket;
+
+
 public class LoginWindow {
+    public TextField username;
+    public PasswordField password;
+    public TextField server;
+
+    @FXML
+    protected void loginAction(ActionEvent actionEvent) {
+        if (username.getText().equals("Alegs") && password.getText().equals("Mags")) {
+            Socket temp = ClientMain.connectToServer(server.getText());
+            Client client = new Client(temp, new User(username.getText()));
+            Thread thread = new Thread(client);
+            thread.start();
+        }
+    }
+
 }
