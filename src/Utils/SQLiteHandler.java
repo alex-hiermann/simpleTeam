d1357f5760b8f2448e8dab8f7f3891485e1b7b73
Alexander Hiermann <8108@htl.rennweg.at>
2021-05-30 23:12:23 +0200

    Updated database structure

--- src/Utils/SQLiteHandler.java
+++ src/Utils/SQLiteHandler.java
@@ -3,7 +3,7 @@ package Utils;
 import java.sql.*;
 
 /**
- * @author sqlitetutorial.net
+ * template used from sqlitetutorial.net
  */
 public class SQLiteHandler {
 
@@ -13,16 +13,12 @@ public class SQLiteHandler {
      * @param fileName the database file name
      */
     public static void createNewDatabase(String fileName) {
-
-        String url = "jdbc:sqlite:C:/ProgramFiles/simpleTeam/db/" + fileName;
-
-        try (Connection conn = DriverManager.getConnection(url)) {
+        try (Connection conn = DriverManager.getConnection(Configuration.ST_DIR_PATH + fileName)) {
             if (conn != null) {
                 DatabaseMetaData meta = conn.getMetaData();
                 System.out.println("The driver name is " + meta.getDriverName());
                 System.out.println("A new database has been created.");
             }
-
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
@@ -32,13 +28,10 @@ public class SQLiteHandler {
      * Create a new table in the test database
      */
     public static void createDefaultTables() {
-        // SQLite connection string
-        String url = "jdbc:sqlite:C:/ProgramFiles/simpleTeam/db/simpleTeam.db";
-
         // SQL statement for creating a new table
         String sql = """
-                CREATE TABLE IF NOT EXISTS st_User (
-                	pk_user_id INT PRIMARY KEY,
+                CREATE TABLE IF NOT EXISTS User (
+                	pk_user_id INT PRIMARY KEY NOT NULL,
                 	username CHAR(32) NOT NULL,
                 	name CHAR(32) NOT NULL,
                 	lastname CHAR(32) NOT NULL,
@@ -46,18 +39,63 @@ public class SQLiteHandler {
                 	birth DATE NOT NULL,
                 	password CHAR(20) NOT NULL
                 );
-                CREATE TABLE IF NOT EXISTS st_Team (
-                	pk_team_id INT PRIMARY KEY,
+                CREATE TABLE IF NOT EXISTS Chatroom (
+                	pk_chatroom_id INT PRIMARY KEY NOT NULL
+                );
+                CREATE TABLE IF NOT EXISTS Message (
+                	pk_message_id INT PRIMARY KEY NOT NULL,
+                	text TEXT NOT NULL,
+                	date DATE NOT NULL,
+                	fk_pk_user_id INT NOT NULL,
+                	fk_pk_chatroom INT NOT NULL,
+                	FOREIGN KEY (fk_pk_user_id) REFERENCES User (pk_user_id),
+                    FOREIGN KEY (fk_pk_chatroom) REFERENCES Chatroom (pk_chatroom_id)
+                );
+                CREATE TABLE IF NOT EXISTS Team (
+                	pk_team_id INT PRIMARY KEY NOT NULL,
                 	name CHAR(32) NOT NULL,
                 	description CHAR(128) NOT NULL,
                 	fk_admin_id INT NOT NULL,
-                	fk_chatroom_id INT NOT NULL
+                	fk_pk_chatroom_id INT NOT NULL,
+                	FOREIGN KEY (fk_admin_id) REFERENCES User (pk_user_id),
+                    FOREIGN KEY (fk_pk_chatroom_id) REFERENCES Chatroom (pk_chatroom_id)
+                );
+                CREATE TABLE IF NOT EXISTS Team_User (
+                    fk_pk_team_id INT NOT NULL,
+                    fk_pk_user_id INT NOT NULL,
+                    FOREIGN KEY (fk_pk_team_id) REFERENCES Team (pk_team_id),
+                    FOREIGN KEY (fk_pk_user_id) REFERENCES User (pk_user_id)
                 );
                 """;
 
-        try (Connection conn = DriverManager.getConnection(url);
+//        TODO Add table "Task" to the database structure, template:
+//
+//        CREATE TABLE IF NOT EXISTS Task (
+//                pk_task_id INT PRIMARY KEY NOT NULL,
+//                name CHAR(32) NOT NULL,
+//        description CHAR(128) NOT NULL,
+//        fk_admin_id INT NOT NULL,
+//                );
+//
+//        TODO Add table for the connection between "Task" and "User" to the database structure, template:
+//
+//        CREATE TABLE IF NOT EXISTS Task_User (
+//                fk_pk_task_id INT NOT NULL,
+//                fk_pk_user_id INT NOT NULL,
+//                );
+//
+//        TODO Add table "Serverconfig" to the database structure, template:
+//
+//        CREATE TABLE IF NOT EXISTS Serverconfig (
+//                pk_srvconf_id INT PRIMARY KEY NOT NULL,
+//                ???
+//                ???
+//                ???
+//                ???
+//                );
+//
+        try (Connection conn = DriverManager.getConnection(Configuration.DATABASE_URL);
              Statement stmt = conn.createStatement()) {
-            // create a new table
             stmt.execute(sql);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
