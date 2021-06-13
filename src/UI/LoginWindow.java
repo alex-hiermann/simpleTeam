604edb0aba8b6f3c3c618f4c4d183a0b64085c9e
Maximilian Burger <mabug@outlook.de>
2021-06-13 17:27:11 +0200

    Made a regex checker for login window

--- src/UI/LoginWindow.java
+++ src/UI/LoginWindow.java
@@ -4,18 +4,19 @@ import Client.ClientMain;
 import Client.Client;
 import Client.User;
 import Utils.BasicFunctionLibrary;
+import Utils.Configuration;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class LoginWindow {
@@ -49,6 +50,28 @@ public class LoginWindow {
 
     public void extracted() {
         //        button.setDisable(true);
+        Matcher matcher = Pattern.compile(Configuration.CHECK_IP_AND_PORT_REGEX).matcher(server.getText());
+        Matcher emailMatcher = Pattern.compile(Configuration.CHECK_EMAIL_REGEX).matcher(email.getText());
+        if (!matcher.matches()) {
+            Platform.runLater(() -> {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Server IP is invalid!");
+                alert.setContentText("Try again!");
+                alert.showAndWait();
+            });
+             return;
+        }
+        if (!emailMatcher.matches()) {
+            Platform.runLater(() -> {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Email is invalid");
+                alert.setContentText("Try again!");
+                alert.showAndWait();
+            });
+            return;
+        }
         Socket temp = ClientMain.connectToServer(server.getText());
         Client client = new Client(temp, new User("tempUser05070201"));
         new Thread(client).start();
