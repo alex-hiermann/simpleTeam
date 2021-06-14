a76831ca6121cc09551fde6c7b65d1555fa1a8c2
Alex Hiermann <8108.htl.rennweg.at>
2021-06-14 12:00:26 +0200

    RegisterWindow last check for bugs and javadocs

--- src/UI/RegisterWindow.java
+++ src/UI/RegisterWindow.java
@@ -6,7 +6,6 @@ import Client.User;
 import Utils.BasicFunctionLibrary;
 import Utils.Configuration;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
@@ -18,38 +17,81 @@ import java.net.Socket;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Class for the register window which is used by the user to create a new user profile
+ * <p>
+ * Created and modified by Burger Maximilian and Hiermann Alexander.
+ * Please consider correct usage of the LICENSE.
+ */
 public class RegisterWindow {
 
+    /**
+     * Datepicker for the date
+     */
     public DatePicker date;
+
+    /**
+     * TextField for the lastname
+     */
     public TextField lastname;
+
+    /**
+     * TextField for the name
+     */
     public TextField name;
+
+    /**
+     * PasswordField for the password
+     */
     public PasswordField password;
+
+    /**
+     * TextField for the email
+     */
     public TextField email;
+
+    /**
+     * TextField for the username
+     */
     public TextField username;
+
+    /**
+     * TextField for the server
+     */
     public TextField server;
+
+    /**
+     * AnchorPane for the pane
+     */
     public AnchorPane pane;
 
+    /**
+     * initialize method called to create a textfield-keylistener
+     */
     public void initialize() {
         username.setOnKeyPressed(keyEvent -> {
             if (keyEvent.getCode() == KeyCode.ENTER) {
-                register(new ActionEvent());
+                register();
             }
         });
 
         password.setOnKeyPressed(keyEvent -> {
             if (keyEvent.getCode() == KeyCode.ENTER) {
-                register(new ActionEvent());
+                register();
             }
         });
 
         server.setOnKeyPressed(keyEvent -> {
             if (keyEvent.getCode() == KeyCode.ENTER) {
-                register(new ActionEvent());
+                register();
             }
         });
     }
 
-    public void register(ActionEvent actionEvent) {
+    /**
+     * used to register a new user profile using STRequests and alerts
+     */
+    public void register() {
 
         Matcher serverMatcher = Pattern.compile(Configuration.CHECK_IP_AND_PORT_REGEX).matcher(server.getText());
         Matcher emailMatcher = Pattern.compile(Configuration.CHECK_EMAIL_REGEX).matcher(email.getText());
@@ -64,6 +106,7 @@ public class RegisterWindow {
             });
             return;
         }
+
         if (!emailMatcher.matches()) {
             Platform.runLater(() -> {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -74,7 +117,6 @@ public class RegisterWindow {
             });
             return;
         }
-
 
         User tempUser = new User(username.getText(), name.getText(), lastname.getText(), email.getText(), date.getValue(), BasicFunctionLibrary.hashPassword(password.getText()));
         Socket temp = ClientMain.connectToServer(server.getText());
