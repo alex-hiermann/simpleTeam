3fa3084f138553499a7ad2a0e1aae05c346e5458
Alex Hiermann <8108.htl.rennweg.at>
2021-06-14 09:10:09 +0200

    ClientMain last check for bugs and javadocs

--- src/Client/ClientMain.java
+++ src/Client/ClientMain.java
@@ -2,47 +2,73 @@ package Client;
 
 import UI.AddNewTaskWindow;
 import UI.InviteUserWindow;
-import UI.LoginWindow;
+import UI.MainWindow;
 import Utils.Configuration;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
-import UI.MainWindow;
-
 
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Objects;
 
+/**
+ * Class to create and start all used FXML files for the graphical user interface
+ *
+ * Created and modified by Burger Maximilian and Hiermann Alexander.
+ * Please consider correct usage of the LICENSE.
+ */
 public class ClientMain extends Application {
 
+    /**
+     * Stage of the window
+     */
     public static Stage currentStage = new Stage();
 
+    /**
+     * Client which uses the gui
+     */
     public static Client client;
 
-    private static FXMLLoader main;
-
+    /**
+     * Defines the MainWindow to access it from other Classes
+     */
     public static MainWindow mainWindow;
 
+    /**
+     * main method to start the first window
+     * @param args args for javafx and FXML launches
+     */
     public static void main(String[] args) {
         System.out.println(Configuration.ANSI_GREEN + "Initializing Simple Team" + Configuration.ANSI_RESET);
         launch(args);
     }
 
+    /**
+     * used to connect the client to the server using the IP Address
+     * @param ip string of ip address
+     * @return socket used to connect to the server
+     */
     public static Socket connectToServer(String ip) {
         try {
             Socket s = new Socket(ip.split(":")[0], Integer.parseInt(ip.split(":")[1]));
             System.out.println(Configuration.ANSI_GREEN + "Connection successful: " + s + Configuration.ANSI_RESET);
             return s;
         } catch (Exception ignored) {
+            System.out.println(Configuration.ANSI_GREEN + "Invalid IP Address" + Configuration.ANSI_RESET);
         }
         return null;
     }
 
+    /**
+     * starts after first window has been initialized
+     * login window used to login
+     * @param stage where your scene should be build in
+     * @throws Exception exception thrown when FXML errors appear
+     */
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/UI/LoginWindow.fxml")));
@@ -55,6 +81,10 @@ public class ClientMain extends Application {
         currentStage = stage;
     }
 
+    /**
+     * login window used to login
+     * @throws Exception exception thrown when FXML errors appear
+     */
     public void showLoginWindow() throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/UI/LoginWindow.fxml")));
         Scene scene = new Scene(root);
@@ -67,6 +97,10 @@ public class ClientMain extends Application {
         currentStage = stage;
     }
 
+    /**
+     * register window to register a new user profile
+     * @throws IOException exception thrown when FXML errors appear
+     */
     public void showRegisterWindow() throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/UI/RegisterWindow.fxml")));
         Stage stage = new Stage();
@@ -77,8 +111,12 @@ public class ClientMain extends Application {
         currentStage = stage;
     }
 
+    /**
+     * main window where most of the things are displayed
+     * @throws IOException exception thrown when FXML errors appear
+     */
     public void showMainWindow() throws IOException {
-        main = new FXMLLoader(getClass().getResource("/UI/MainWindow.fxml"));
+        FXMLLoader main = new FXMLLoader(getClass().getResource("/UI/MainWindow.fxml"));
         Parent root = main.load();
         mainWindow = main.getController();
         Stage stage = new Stage();
@@ -89,6 +127,10 @@ public class ClientMain extends Application {
         currentStage = stage;
     }
 
+    /**
+     * create team window used to create a new team
+     * @throws IOException exception thrown when FXML errors appear
+     */
     public void showAddTeamWindow() throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/UI/AddTeamWindow.fxml")));
         Stage stage = new Stage();
@@ -100,6 +142,11 @@ public class ClientMain extends Application {
         currentStage = stage;
     }
 
+    /**
+     * invite user window used to invite a user to a team
+     * @param team team where the user should be invited to
+     * @throws IOException exception thrown when FXML errors appear
+     */
     public void showInviteUserWindow(Team team) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/UI/InviteUserWindow.fxml")));
         Stage stage = new Stage();
@@ -112,6 +159,12 @@ public class ClientMain extends Application {
         currentStage = stage;
     }
 
+    /**
+     * new task window to create a new task
+     * @param team the team where this task should be assigned to
+     * @param admin if this client.user is the admin or not
+     * @throws IOException exception thrown when FXML errors appear
+     */
     public void showAddNewTaskWindow(Team team, boolean admin) throws IOException {
         AddNewTaskWindow.team = team;
         AddNewTaskWindow.admin = admin;
