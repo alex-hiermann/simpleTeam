19e7983f5cd69b2afe659fa6fc3b5b9dfb3fd036
Alex Hiermann <8108.htl.rennweg.at>
2021-06-14 10:57:12 +0200

    AddTeamWindow last check for bugs and javadocs

--- src/UI/AddTeamWindow.java
+++ src/UI/AddTeamWindow.java
@@ -2,29 +2,55 @@ package UI;
 
 import Client.Client;
 import Client.Team;
-import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 
+/**
+ * Class for the Team Window
+ *
+ * Created and modified by Burger Maximilian and Hiermann Alexander.
+ * Please consider correct usage of the LICENSE.
+ */
 public class AddTeamWindow {
 
+    /**
+     * Textfield for the teamname
+     */
     public TextField teamname;
+
+    /**
+     * Textarea for the teamdesc
+     */
     public TextArea teamdesc;
+
+    /**
+     * AnchorPane for the pane
+     */
     public AnchorPane pane;
+
+    /**
+     * Button for the button
+     */
     public Button button;
 
+    /**
+     * initialize method called to create a textfield-keylistener
+     */
     public void initialize() {
         teamname.setOnKeyPressed(keyEvent -> {
             if (keyEvent.getCode() == KeyCode.ENTER) {
-                createTeam(new ActionEvent());
+                createTeam();
             }
         });
     }
 
-    public void createTeam(ActionEvent actionEvent) {
+    /**
+     * create a new team with sending a STRequest
+     */
+    public void createTeam() {
         if ((!teamname.getText().isEmpty() && !teamname.getText().isBlank())) {
             Team tempTeam = new Team(teamname.getText(), teamdesc.getText());
             tempTeam.setAdmin(Client.user);
