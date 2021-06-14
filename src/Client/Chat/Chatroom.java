cfa223826d03574816518df3ee81918c9c40145b
Alex Hiermann <8108.htl.rennweg.at>
2021-06-14 08:26:23 +0200

    Chatroom last check for bugs and javadocs

--- src/Client/Chat/Chatroom.java
+++ src/Client/Chat/Chatroom.java
@@ -5,11 +5,25 @@ import Client.Team;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+/**
+ * Chatroom class, used to identify the chatroom, used in the database and UI for each team
+ */
 public class Chatroom {
 
+    /**
+     * List of messages used to store all messages, which have been sent in a chatroom
+     */
     public final LinkedList<Message> messages = new LinkedList<>();
+
+    /**
+     * Saves the Team, where this chatroom belongs to
+     */
     private Team team;
 
+    /**
+     * used to get all messages for a STRequest with specific output
+     * @return formatted list of messages for STRequests
+     */
     public ArrayList<String> generateMessages() {
         ArrayList<String> generatedMessages = new ArrayList<>();
         final StringBuilder[] stringBuilder = {new StringBuilder()};
@@ -20,26 +34,47 @@ public class Chatroom {
         return generatedMessages;
     }
 
+    /**
+     * constructor used to initialize a new chatroom with a team
+     * @param team reference to where the chatroom belongs to
+     */
     public Chatroom(Team team) {
         this.team = team;
     }
 
+    /**
+     * deletes the last written message
+     */
     public void deleteLastMessage() {
         this.messages.removeLast();
     }
 
+    /**
+     * adds a new message to the message list
+     * @param message message that will be added
+     */
     public void addMessage(Message message) {
         this.messages.add(message);
     }
 
+    /**
+     * @return list of messages
+     */
     public LinkedList<Message> getMessages() {
         return messages;
     }
 
+    /**
+     * @return team
+     */
     public Team getTeam() {
         return team;
     }
 
+    /**
+     * set the team, where this chatroom is paired with
+     * @param team team to be changed
+     */
     public void setTeam(Team team) {
         this.team = team;
     }
